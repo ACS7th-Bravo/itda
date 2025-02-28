5d17cd81ff97ebe5a09d87e39829909c7b881d95
sonjunhyun <leonica0429@gmail.com>
2025-02-28 18:54:32 +0900

    ci test

--- Frontend/bravo-front/src/routes/about/+page.svelte
+++ Frontend/bravo-front/src/routes/about/+page.svelte
@@ -2,4 +2,4 @@
 <h1>this is about page</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <!-- CI/CD Test -->
-<p>CI/CD Test 2025.02.28 18:45</p>
+<p>CI/CD Test 2025.02.28 18:54</p>
