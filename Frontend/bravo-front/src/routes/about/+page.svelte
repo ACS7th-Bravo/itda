ba2146eeee131a5e419344db01a3c1bc7408ce97
sonjunhyun <leonica0429@gmail.com>
2025-02-28 15:46:59 +0900

    gitlab-ci 수정

--- Frontend/bravo-front/src/routes/about/+page.svelte
+++ Frontend/bravo-front/src/routes/about/+page.svelte
@@ -1,3 +1,5 @@
 <!-- /bravo-front/src/routes/about/+page.svelte -->
 <h1>this is about page</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<!-- CI/CD Test -->
+<p>CI/CD Test</p>
