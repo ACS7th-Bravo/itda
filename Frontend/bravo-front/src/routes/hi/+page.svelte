6cace67ab38f5792cc4eaf2be370528eb7c0aeeb
sonjunhyun <leonica0429@gmail.com>
2025-03-11 22:44:19 +0900

    from gitlab, sonarqube, and ecr to slack notification test

--- Frontend/bravo-front/src/routes/hi/+page.svelte
+++ Frontend/bravo-front/src/routes/hi/+page.svelte
@@ -1,3 +1,3 @@
 <!-- /bravo-front/src/routes/hi/+page.svelte -->
-<h1>this is hi page - GitLab SonarQube Noti Test2</h1>
+<h1>this is hi page - GitLab SonarQube Noti Test3</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
