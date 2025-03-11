f8d63c2929d8c3e10399b5f0b9d06596f6ade101
sonjunhyun <leonica0429@gmail.com>
2025-03-11 20:34:55 +0900

    aws ci test-2

--- Frontend/bravo-front/src/routes/hi/+page.svelte
+++ Frontend/bravo-front/src/routes/hi/+page.svelte
@@ -1,3 +1,3 @@
 <!-- /bravo-front/src/routes/hi/+page.svelte -->
-<h1>this is hi page - CI Test 250311</h1>
+<h1>this is hi page - CI Test 20:34</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
